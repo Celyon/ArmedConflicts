ee036a1eeb366e23ab0ed43ffbac43d91ae89ac8
Celyon <dingfangyu2014@gmail.com>
2020-08-12 13:01:46 +0800

    Program

---
+++ Program/CreatDir.R
@@ -0,0 +1,20 @@
+path <- "E:"
+setwd(path)
+dir.create("E:\\Conflict")
+
+setwd("E:\\Conflict")
+
+dir.create("Graphic")
+dir.create("Model")
+dir.create("Product")
+dir.create("Tables")
+
+dir.create("Product\\step1")
+dir.create("Product\\step2")
+
+dir.create("Tables\\Performace")
+dir.create("Tables\\Accuracy")
+
+dir.create("Tables\\Samples-Revisedversion")
+dir.create("Tables\\Simulation")
+dir.create("Tables\\StatisticalTest")
